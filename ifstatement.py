3af6fa06b40a922c8c6c8456368528c8f479a345
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-15 04:02:29 +0300

    Initial Commit

---
+++ ifstatement.py
@@ -0,0 +1,47 @@
+temperatureiswarm=False
+if temperatureiswarm:
+    print('bell ring')
+else:
+    print('bell does not ring')
+
+
+#traffick light
+colour="white"
+if colour=="red":
+    print("stop")
+elif colour=="green":
+    print("go")
+elif colour=="yellow":
+    print("get ready")
+else:
+    print("invalid")
+
+squid_game="nmjlkdg_light"
+if squid_game=="red_light":
+    print("elimination")
+elif squid_game=="green_light":
+    print("go")
+else:
+    print("get eliminated also")
+
+climate="SPRING".lower()
+if climate=="winter":
+    print("stay indoors")
+elif climate=="summer":
+    print("go to the beach")
+elif climate=="spring":
+    print("join school")
+elif climate=="autumn":
+    print("visit Africa")
+else:
+    print("invalid climate")
+
+age=1456876
+if age>=9 and age<=18:
+    print("energy\ntime\nno money\nlittle thinking")
+elif age>=19 and age<35:
+    print("energy.\n\ttime.\n\tno money\nbig expectations")
+elif age>=35:
+    print("less energy\nno time\nmoney\nmeet expectations")
+else:
+    print("end")
